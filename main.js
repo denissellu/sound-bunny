6b614c9d497e35b5b7fafc44076fc9603288a88a
denissellu <hingasellu@gmail.com>
2016-12-19 23:55:39 +0000

    Add folder and structure and move the index.html, also add settings

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const electron = require('electron')
 const {app, BrowserWindow} = electron
 
 app.on('ready', () => {
-    let win = new BrowserWindow({width: 800, height: 600})
-      win.loadURL(`file://${__dirname}/index.html`)
+    let win = new BrowserWindow({
+      height: 720,
+      width: 368,
+      frame: false,
+      resizable: false,
+    })
+    win.loadURL(`file://${__dirname}/app/index.html`)
 })
